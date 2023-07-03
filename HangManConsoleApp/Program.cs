2e45da3e617a88fd200bc9235d708a278388ea26
AllenOtto <kephamoturi@gmail.com>
2023-07-04 01:16:16 +0300

    Added a PrintHangMan() method with the hangman in different states based on how many wrong guesses the user has entered.

--- HangManConsoleApp/Program.cs
+++ HangManConsoleApp/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿//using System;
+//using System.Collections.Generic;
+//using static System.Random;
+//using System.Text;
+
+static void PrintHangMan(int wrongGuesses)
+{
+    if(wrongGuesses == 1)
+    {
+        Console.WriteLine("\n+---+");
+        Console.WriteLine(" O  |");
+        Console.WriteLine("    |");
+        Console.WriteLine("    |");
+        Console.WriteLine("   ===");
+    }
+    else if(wrongGuesses == 2)
+    {
+        Console.WriteLine("\n+---+");
+        Console.WriteLine(" O  |");
+        Console.WriteLine(" |  |");
+        Console.WriteLine("    |");
+        Console.WriteLine("   ===");
+    }
+    else if(wrongGuesses == 3)
+    {
+        Console.WriteLine("\n+---+");
+        Console.WriteLine(" O  |");
+        Console.WriteLine("/|  |");
+        Console.WriteLine("    |");
+        Console.WriteLine("   ===");
+    }
+    else if(wrongGuesses == 4)
+    {
+        Console.WriteLine("\n+---+");
+        Console.WriteLine(" O   |");
+        Console.WriteLine("/|\\ |");
+        Console.WriteLine("     |");
+        Console.WriteLine("   ===");
+    }
+    else if(wrongGuesses == 5)
+    {
+        Console.WriteLine("\n+---+");
+        Console.WriteLine(" O   |");
+        Console.WriteLine("/|\\ |");
+        Console.WriteLine("/    |");
+        Console.WriteLine("   ===");
+    }
+    else if(wrongGuesses == 6)
+    {
+        Console.WriteLine("\n+---+");
+        Console.WriteLine(" O   |");
+        Console.WriteLine("/|\\ |");
+        Console.WriteLine("/ \\ |");
+        Console.WriteLine("   ===");
+    }
+}
+
